e0c66aa3febcfd72648c8d9dc3f93426b61c6344
Aleksandr Ivanishchev <ua9lif@mail.ru>
2023-09-12 15:36:22 +0500

    checked udp trx

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,11 @@
 
 #define LED 2
 
-#define WIFI_AP
+//UDP port
+const uint16_t port = 7777;
+
+//Access point or client
+//#define WF_AP
 
 // include the library
 #include <RadioLib.h>
@@ -30,16 +34,13 @@ float TX_freq = 262.225+33.6;
 
 SX1262 radio = new Module(5, 21, 33, 26);
 
+AsyncUDP udp;
 
-AsyncWebServer server(80);
-
-
-#ifdef WIFI_AP
+#ifdef WF_AP
   const char* ssid = "LoraSat";          // Your WiFi SSID
 #else
   const char* ssid = "cv_home";          // Your WiFi SSID
 #endif
-
 const char* password = "naksitral";  // Your WiFi Password
 
 // save transmission states between loops
@@ -62,26 +63,49 @@ void setFlag(void) {
 }
 
 
+
 void transmit(String &data){
   // turn LED on
   digitalWrite(2, HIGH);  // turn the LED on (HIGH is the voltage level)
   
-  WebSerial.println(F("[SX1262] Transmitting packet ... "));
+  Serial.println(F("[SX1262] Transmitting packet ... "));
   //int state = radio.startTransmit(data);
   radio.setFrequency(TX_freq);
   transmissionState = radio.startTransmit(data);
   transmitFlag = true;
 }
 
+void parsePacket(AsyncUDPPacket packet)
+{
 
-void recvMsg(uint8_t *data, size_t len){
-  WebSerial.println("Received Data...");
-  String d = "";
-  for(int i=0; i < len; i++){
-    d += char(data[i]);
-  }
-  WebSerial.println(d);
-  transmit(d);
+  Serial.print("UDP Packet From: ");
+  Serial.print(packet.remoteIP());
+  Serial.print(":");
+  Serial.print(packet.remotePort());
+  Serial.print(", Data: ");
+  Serial.write(packet.data(), packet.length());
+  Serial.println();
+
+  // https://stackoverflow.com/questions/58253174/saving-packet-data-to-string-with-asyncudp-on-esp32
+  // String  mensaje = (char*)(packet.data());
+  char* tmpStr = (char*) malloc(packet.length() + 1);
+  memcpy(tmpStr, packet.data(), packet.length());
+  tmpStr[packet.length()] = '\0'; // ensure null termination
+  String message = String(tmpStr);
+  free(tmpStr); // Strign(char*) creates a copy so we can delete our one
+
+  Serial.println(message);
+  if(message == "on"){
+    digitalWrite(2, HIGH);
+    packet.print("Received: on");
+    }
+  if(message == "off"){
+    digitalWrite(2, LOW);
+    packet.print("Received: off");
+    }
+
+  //send to lora
+  transmit(message);
 }
 
 void setup() {
@@ -92,27 +116,28 @@ void setup() {
   Serial.print("[WiFi] Initializing...");
   // Remove the password parameter, if you want the AP (Access Point) to be open
 
-#ifdef WIFI_AP
-  WiFi.softAP(ssid, password);
+  #ifdef WF_AP
+    WiFi.softAP(ssid, password);
 
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
-#else
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.printf("WiFi Failed!\n");
-    return;
-  }
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-#endif
-
-  //WebSerial is accessible at "<IP Address>/webserial" in browser
-  WebSerial.begin(&server);
-  WebSerial.msgCallback(recvMsg);
-  server.begin();
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+  #else
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+      Serial.printf("WiFi Failed!\n");
+      return;
+    }
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+  #endif
+  
+  
+  if(udp.listen(port)) {
+        // При получении пакета вызываем callback функцию
+        udp.onPacket(parsePacket);
+    }
 
   // initialize SX1262 with default settings
   Serial.print(F("[SX1262] Initializing ... "));
@@ -132,9 +157,6 @@ void setup() {
 
   radio.setPacketSentAction(setFlag);
   radio.setPacketReceivedAction(setFlag);
-
-  // set the function that will be called
-  // when new packet is received
   //radio.setDio1Action(setFlag);
 
   Serial.print(F("[SX1262] Starting to listen ... "));
@@ -148,7 +170,6 @@ void setup() {
   }
 }
 
-
 void loop() {  
 // check if the previous operation finished
   if(operationDone) {
@@ -160,11 +181,11 @@ void loop() {
       // print the result
       if (transmissionState == RADIOLIB_ERR_NONE) {
         // packet was successfully sent
-        WebSerial.println(F("transmission finished!"));        
+        Serial.println(F("transmission finished!"));        
 
       } else {
-        WebSerial.print(F("failed, code "));
-        WebSerial.println(transmissionState);
+        Serial.print(F("failed, code "));
+        Serial.println(transmissionState);
 
       }
       //radio.finishTransmit();
@@ -185,30 +206,30 @@ void loop() {
 
       if (state == RADIOLIB_ERR_NONE) {
         // packet was successfully received
-        WebSerial.println(F("[SX1262] Received packet!"));
+        Serial.println(F("[SX1262] Received packet!"));
 
         // print data of the packet
-        WebSerial.print(F("[SX1262] Data:\t\t"));
-        WebSerial.println(str);
+        Serial.print(F("[SX1262] Data:\t\t"));
+        Serial.println(str);
 
         // print RSSI (Received Signal Strength Indicator)
-        WebSerial.print(F("[SX1262] RSSI:\t\t"));
-        WebSerial.print(radio.getRSSI());
-        WebSerial.println(F(" dBm"));
+        Serial.print(F("[SX1262] RSSI:\t\t"));
+        Serial.print(radio.getRSSI());
+        Serial.println(F(" dBm"));
 
         // print SNR (Signal-to-Noise Ratio)
-        WebSerial.print(F("[SX1262] SNR:\t\t"));
-        WebSerial.print(radio.getSNR());
-        WebSerial.println(F(" dB"));
-
+        Serial.print(F("[SX1262] SNR:\t\t"));
+        Serial.print(radio.getSNR());
+        Serial.println(F(" dB"));
+        //send UDP
+        udp.broadcast(str.c_str());
       }    
       else {
-        WebSerial.print(F("failed, code "));
-        WebSerial.println(state);
-    
+        Serial.print(F("failed, code "));
+        Serial.println(state);    
       }     
     }
-    // wait a second before transmitting again
-      delay(1000);     
+    // wait before transmitting again
+      delay(500);     
   }  
 }
