4f1e127c3d258fa5b167000da475492d91d09953
Aleksandr Ivanishchev <ua9lif@mail.ru>
2023-09-13 11:36:45 +0500

    Work BLE

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,22 @@
 */
 
 #include <Arduino.h>
-#include <WiFi.h>
-#include <ESPTelnet.h>
-
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
 #define LED 2
 
-//telnet port
-const uint16_t port = 23;
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-//Access point or client
-//#define WF_AP
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+
+String bt_rec;
+String bt_send;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
 
 // include the library
 #include <RadioLib.h>
@@ -35,16 +40,6 @@ float TX_freq = 262.225+33.6 + Freq_error;
 
 
 SX1262 radio = new Module(5, 21, 33, 26);
-
-ESPTelnet telnet;
-IPAddress ip;
-
-#ifdef WF_AP
-  const char* ssid = "LoraSat";          // Your WiFi SSID
-#else
-  const char* ssid = "cv_home";          // Your WiFi SSID
-#endif
-const char* password = "naksitral";  // Your WiFi Password
 
 // save transmission states between loops
 int transmissionState = RADIOLIB_ERR_NONE;
@@ -66,7 +61,6 @@ void setFlag(void) {
 }
 
 
-
 void transmit(String &data){
   // turn LED on
   digitalWrite(2, HIGH);  // turn the LED on (HIGH is the voltage level)
@@ -78,95 +72,74 @@ void transmit(String &data){
   transmitFlag = true;
 }
 
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
 
-void onTelnetConnect(String ip) {
-  Serial.print("- Telnet: ");
-  Serial.print(ip);
-  Serial.println(" connected");
-  
-  telnet.println("\nWelcome " + telnet.getIP());
-  telnet.println("(Use ^] + q  to disconnect.)");
-}
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
 
-void onTelnetDisconnect(String ip) {
-  Serial.print("- Telnet: ");
-  Serial.print(ip);
-  Serial.println(" disconnected");
-}
+class MyCallbacks: public BLECharacteristicCallbacks {   
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+     
+	  if (value.length() > 0) {
+        bt_rec = "";
+        for (int i = 0; i < value.length(); i++){
+          // Serial.print(value[i]);
+          bt_rec = bt_rec + value[i];
+        }
 
-void onTelnetReconnect(String ip) {
-  Serial.print("- Telnet: ");
-  Serial.print(ip);
-  Serial.println(" reconnected");
-}
-
-void onTelnetConnectionAttempt(String ip) {
-  Serial.print("- Telnet: ");
-  Serial.print(ip);
-  Serial.println(" tried to connected");
-}
-
-void onTelnetInput(String str) {
-  // checks for a certain command
-  if (str == "ping") {
-    telnet.println("> pong");
-    Serial.println("- Telnet: pong");
-  // disconnect the client
-  } else if (str == "bye") {
-    telnet.println("> disconnecting you...");
-    telnet.disconnectClient();
-    } else {
-    //send to lora
-    transmit(str);
-   }
-  }
-
-void setupTelnet() {  
-  // passing on functions for various telnet events
-  telnet.onConnect(onTelnetConnect);
-  telnet.onConnectionAttempt(onTelnetConnectionAttempt);
-  telnet.onReconnect(onTelnetReconnect);
-  telnet.onDisconnect(onTelnetDisconnect);
-  telnet.onInputReceived(onTelnetInput);
-
-  Serial.print("- Telnet: ");
-  if (telnet.begin(port)) {
-    Serial.println("running");
-  } else {
-    Serial.println("error.");   
-  }
-}
+        Serial.print("BLE Received = ");
+        Serial.println(bt_rec);
+         //send to lora
+        transmit(bt_rec);
+		
+        /*if(bt_rec == "on") {
+          //turn LED on
+          digitalWrite(2, HIGH);   // turn the LED on by making the voltage HIGH
+          //pCharacteristic->setValue(bt_send1);
+          }
+        if(bt_rec == "off") {
+          //turn LED off
+          digitalWrite(2, LOW);   // turn the LED off by making the voltage LOW 
+          //pCharacteristic->setValue(bt_send2.c_str()); 
+          }        
+        */
+      }
+    }
+};
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
-  
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("[WiFi] Initializing...");
-  // Remove the password parameter, if you want the AP (Access Point) to be open
 
-  #ifdef WF_AP
-    WiFi.softAP(ssid, password);
+  BLEDevice::init("ESP32 Lora");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
 
-    IPAddress IP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(IP);
-  #else
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-      Serial.printf("WiFi Failed!\n");
-      return;
-    }
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
-  #endif
-  
-  setupTelnet();
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pCharacteristic->setValue("Init.");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+
 
   // initialize SX1262 with default settings
   Serial.print(F("[SX1262] Initializing ... "));
 
+  //7.81 , 10.42 , 15.63, 20.83 
+  //20.83
   //int state = radio.begin(262.225+33.6, 15.63, 9, 5, 0x34, 22, 8, 0); // works
   //int state = radio.begin(262.226+33.6, 20.83, 9, 5, 0x34, 22, 8, 0); // works
   int state = radio.begin(RX_freq, 20.83, 9, 5, 0x34, 22, 8, 0); // works
@@ -195,69 +168,85 @@ void setup() {
   }
 }
 
-void loop() {
-  telnet.loop();  
-// check if the previous operation finished
-  if(operationDone) {
-    // reset flag
-    operationDone = false;
+void loop() {  
+  if (deviceConnected) {    
+    // check if the previous operation finished
+    if(operationDone) {
+      // reset flag
+      operationDone = false;
 
-    if(transmitFlag) {
-      // the previous operation was transmission, listen for response
-      // print the result
-      if (transmissionState == RADIOLIB_ERR_NONE) {
-        // packet was successfully sent
-        Serial.println(F("transmission finished!"));        
+      if(transmitFlag) {
+        // the previous operation was transmission, listen for response
+        // print the result
+        if (transmissionState == RADIOLIB_ERR_NONE) {
+          // packet was successfully sent
+          Serial.println(F("transmission finished!"));        
+
+        } else {
+          Serial.print(F("TX failed, code "));
+          Serial.println(transmissionState);
+          bt_send = "TX failed";
+
+        }
+        //radio.finishTransmit();
+        //turn LED off
+        digitalWrite(2, LOW);   // turn the LED off by making the voltage LOW        
+        radio.setFrequency(RX_freq);
+        
+        // listen for response
+        radio.startReceive();
+        transmitFlag = false;
 
       } else {
-        Serial.print(F("TX failed, code "));
-        Serial.println(transmissionState);
-        telnet.println("TX failed");
+        // the previous operation was reception
+        // print data and send another packet
+        String str;
+        int state = radio.readData(str);
 
+
+        if (state == RADIOLIB_ERR_NONE) {
+          // packet was successfully received
+          Serial.println(F("[SX1262] Received packet!"));
+
+          // print data of the packet
+          Serial.print(F("[SX1262] Data:\t\t"));
+          Serial.println(str);
+
+          // print RSSI (Received Signal Strength Indicator)
+          Serial.print(F("[SX1262] RSSI:\t\t"));
+          Serial.print(radio.getRSSI());
+          Serial.println(F(" dBm"));
+
+          // print SNR (Signal-to-Noise Ratio)
+          Serial.print(F("[SX1262] SNR:\t\t"));
+          Serial.print(radio.getSNR());
+          Serial.println(F(" dB"));
+          //send to client
+          bt_send = str;
+        }    
+        else {
+          Serial.print(F("RX failed, code "));
+          Serial.println(state);
+          bt_send = "RX failed";
+        }     
       }
-      //radio.finishTransmit();
-      //turn LED off
-      digitalWrite(2, LOW);   // turn the LED off by making the voltage LOW        
-      radio.setFrequency(RX_freq);
-      
-      // listen for response
-      radio.startReceive();
-      transmitFlag = false;
-
-    } else {
-      // the previous operation was reception
-      // print data and send another packet
-      String str;
-      int state = radio.readData(str);
-
-
-      if (state == RADIOLIB_ERR_NONE) {
-        // packet was successfully received
-        Serial.println(F("[SX1262] Received packet!"));
-
-        // print data of the packet
-        Serial.print(F("[SX1262] Data:\t\t"));
-        Serial.println(str);
-
-        // print RSSI (Received Signal Strength Indicator)
-        Serial.print(F("[SX1262] RSSI:\t\t"));
-        Serial.print(radio.getRSSI());
-        Serial.println(F(" dBm"));
-
-        // print SNR (Signal-to-Noise Ratio)
-        Serial.print(F("[SX1262] SNR:\t\t"));
-        Serial.print(radio.getSNR());
-        Serial.println(F(" dB"));
-        //send to client
-        telnet.println(str.c_str());
-      }    
-      else {
-        Serial.print(F("RX failed, code "));
-        Serial.println(state);
-        telnet.println("RX failed");
-      }     
+      //send BLE
+      pCharacteristic->setValue(bt_send.c_str()); 
+      pCharacteristic->notify();
+      delay(5); // bluetooth stack will go into congestion, if too many packets are sent.
+      // wait before transmitting again
+        delay(500);     
+    }  
+  }
+  if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising     
+        Serial.println("start listen BLE");
+        oldDeviceConnected = deviceConnected;   
     }
-    // wait before transmitting again
-      delay(500);     
-  }  
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
 }
